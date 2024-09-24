cad1219fab29dbcb3f986b342f89c1d1b4de5df5
Cecilia <cecilia@letspair.ca>
2024-09-24 10:01:31 -0700

    Carousel completed

---
+++ src/components/Card.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Arrow from '../../public/arrow.svg'
+import { AnimatePresence, motion } from 'framer-motion';
+
+interface CardProps {
+    image: string;
+}
+
+const Card: React.FC<CardProps> = ({ image }) => {
+    const [showOverlay, setShowOverlay] = useState(false);
+    return (
+        <motion.div
+            className="relative overflow-hidden h-[200px] min-w-[200px] bg-slate-400 rounded-xl flex justify-center items-center"
+            onHoverStart={() => setShowOverlay(true)}
+            onHoverEnd={() => setShowOverlay(false)}
+        >
+            <AnimatePresence>
+            {showOverlay && (
+                <motion.div
+                    className="absolute inset-0 z-10 flex justify-center items-center"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                >
+                    <div className="absolute bg-black pointer-events-none opacity-50 h-full w-full"></div>
+                    <motion.h1
+                        className="bg-white font-semibold text-sm z-10 px-3 py-2 rounded-full flex items-center ga-[0.5ch] hover:opacity-75"
+                        initial={{ y: 10 }}
+                        animate={{ y: 0 }}
+                        exit={{ y: 10 }}
+                    >
+                        <span>Explore now</span>
+                        <Arrow className="h-4 w-4" />
+                    </motion.h1>
+                </motion.div>
+            )}
+            </AnimatePresence>
+            <Image src={image} alt={image} fill style={{ objectFit: 'cover' }} />
+        </motion.div>
+    )
+}
+
+export default Card
